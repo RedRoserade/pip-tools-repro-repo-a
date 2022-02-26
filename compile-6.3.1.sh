8517bdbff41970098944169af561222b4df89b09
André Carvalho <afecarvalho@gmail.com>
2022-02-26 18:50:30 +0000

    Add minimal repro files

---
+++ compile-6.3.1.sh
@@ -0,0 +1,3 @@
+pip install -U "pip<22" "pip-tools<=6.3.1"
+
+pip-compile requirements.in --output-file requirements-6.3.1.txt
---
+++ compile-latest.sh
@@ -0,0 +1,3 @@
+pip install -U "pip" "pip-tools"
+
+pip-compile requirements.in --output-file requirements-latest.txt
